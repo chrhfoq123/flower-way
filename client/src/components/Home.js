adeb33005c9526f9d8fe7779fb7c6621a9869f73
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2022-07-02 23:56:44 +0900

    22-07-02 merge csm

---
+++ client/src/components/Home.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Register extends Component
+{
+    render()
+    {
+        return(
+            <div className="home">
+                대충 Home  화면
+            </div>
+        );
+    }
+}
+export default Register;
