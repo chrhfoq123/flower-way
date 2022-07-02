6c632abeb9d753507643e50dca09549e9318e7f8
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2022-07-02 20:59:06 +0900

    22-07-02 Header Add

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import React, {Component} from 'react';
 import './css/App.css';
 import './css/Main.css';
+import menu_icon from './sorce/menu_icon.svg';
 
 class App extends Component
 {
@@ -9,7 +10,18 @@ class App extends Component
   {
     return(
       <div className='app'>
-        
+        {/* Header */}
+        <div className='header'>
+          <img src={menu_icon} width='48px' height='48px' />
+        </div>
+        {/* Header */}
+        {/* Content */}
+        <div className='content'>
+          <div className=''>
+            이제 각 화면에 맞는 구성
+          </div>          
+        </div>
+        {/* Content */}
       </div>
     );
   }
