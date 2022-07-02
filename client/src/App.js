63530262bd8ca9b206b6697a5cf7f9a8d5ad3d51
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2022-07-02 22:38:27 +0900

    22-07-02 Add register Component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import './css/App.css';
 import './css/Main.css';
 import menu_icon from './sorce/menu_icon.svg';
+import Register from './components/Register';
 
 class App extends Component
 {
@@ -13,13 +14,14 @@ class App extends Component
         {/* Header */}
         <div className='header'>
           <img src={menu_icon} width='48px' height='48px' />
+          <span className='title'>
+            회원가입
+          </span>
         </div>
         {/* Header */}
         {/* Content */}
         <div className='content'>
-          <div className=''>
-            이제 각 화면에 맞는 구성
-          </div>          
+          <Register></Register>
         </div>
         {/* Content */}
       </div>
